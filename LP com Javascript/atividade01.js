a9bc81a394b9857c7c699a1e3bb5f40239dde4d2
WagnerPimentel <142364404+WagnerPimentel@users.noreply.github.com>
2023-10-30 20:08:25 -0300

    Pré exercício
    Tudo que precisávamos anotar antes de fazer o exercício

--- LP com Javascript/atividade01.js
+++ LP com Javascript/atividade01.js
@@ -1,2 +1,43 @@
 console.log("Lógica de Programação")
 console.log("com Javascript")
+
+// variável
+/*
+3 formas de declarar uma variável
+    1ª sem declarar
+    2ª utilizando var
+*/
+// 1ª sem declarar
+nome = "Wagner"
+
+// 2ª utilizando var
+var idade = 28
+
+// 3ª utilizando let
+let email = "wagner.pimentel@sp.senac.br"
+
+console.log(nome, idade, email)
+
+
+// constante
+const escola = "Senac Americana"
+
+console.log(escola)
+
+// Tipos de dados
+/*
+String - texto
+Number - int, float
+Boolean - True / False
+function, undefined, null, object, symbol
+*/
+
+console.log("NOME => " + typeof nome)
+console.log("IDADE => " + typeof idade)
+console.log("EMAIL => " + typeof email)
+console.log("ESCOLA => " + typeof escola)
+
+nome = "Wagner"
+let sobrenome = "Pimentel"
+
+console.log("Nome completo: " + nome + " " + sobrenome)
